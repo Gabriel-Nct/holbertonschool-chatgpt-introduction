7ad5eff15f6df62be3c0e990373b5da192650189
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-15 11:16:45 +0100

    Debugging - Python Factorial

---
+++ factorial.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 factorial.py <number>")
+    sys.exit(1)
+
+
+try:
+    n = int(sys.argv[1])
+    if n < 0:
+        print("Please enter a positive integer.")
+        sys.exit(1)
+except ValueError:
+    print("Please enter a valid integer.")
+    sys.exit(1)
+
+
+f = factorial(n)
+print(f)
